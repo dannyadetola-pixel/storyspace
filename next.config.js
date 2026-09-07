82a43c2bba88a684daa5b8b98991a21ed722aba3
dannyadetola-pixel <oluwafemijss2b@gmail.com>
2026-09-08 00:47:21 +0100

    Fix missing pathname in image remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   images: {
     remotePatterns: process.env.SUPABASE_URL
-      ? [{ protocol: "https", hostname: new URL(process.env.SUPABASE_URL).hostname }]
+      ? [
+          {
+            protocol: "https",
+            hostname: new URL(process.env.SUPABASE_URL).hostname,
+            pathname: "/**",
+          },
+        ]
       : [],
   },
 };
