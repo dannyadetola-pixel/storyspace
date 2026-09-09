0dca28059d9a3eda064bb94a0028952cf967cec9
dannyadetola-pixel <oluwafemijss2b@gmail.com>
2026-09-09 10:33:22 +0100

    Restore image config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: process.env.SUPABASE_URL
+      ? [
+          {
+            protocol: "https",
+            hostname: new URL(process.env.SUPABASE_URL).hostname,
+            pathname: "/**",
+          },
+        ]
+      : [],
+  },
+};
 
 module.exports = nextConfig;
